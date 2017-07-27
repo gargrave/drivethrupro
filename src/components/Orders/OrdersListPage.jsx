25719c173c54a4c9bb1a5a2330185edad4d30c74
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-27 13:50:09 -0700

    Connect OrderListPage to store

--- src/components/Orders/OrdersListPage.jsx
+++ src/components/Orders/OrdersListPage.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { array } from 'prop-types'
 
-const OrdersListPage = () => {
-  return (
-    <div>
-      <h2>Orders List Page</h2>
-    </div>
-  )
+class OrdersListPage extends Component {
+  render () {
+    return (
+      <div>
+        <h2>Orders List Page</h2>
+      </div>
+    )
+  }
 }
 
-export default OrdersListPage
+OrdersListPage.propTypes = {
+  orders: array.isRequired
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    orders: state.orders
+  }
+}
+
+export default connect(mapStateToProps)(OrdersListPage)
