7967d94ef4cc7bc75450834f308320aefa3f32ab
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-29 13:04:35 -0700

    Add some menu items. Our restaurant is expanding!

--- src/data/menu-items.js
+++ src/data/menu-items.js
@@ -8,8 +8,33 @@ export default [
   },
   {
     id: nextId++,
+    name: 'Cheeseburger',
+    sizes: [{ name: 'Regular', price: 119 }]
+  },
+  {
+    id: nextId++,
+    name: 'Double Cheeseburger',
+    sizes: [{ name: 'Regular', price: 189 }]
+  },
+  {
+    id: nextId++,
+    name: 'SUPER ULTRA Burger',
+    sizes: [{ name: 'Regular', price: 399 }]
+  },
+  {
+    id: nextId++,
+    name: 'Chicken Sandwich',
+    sizes: [{ name: 'Regular', price: 179 }]
+  },
+  {
+    id: nextId++,
     name: 'French Fries',
     sizes: [{ name: 'Small', price: 49 }, { name: 'Medium', price: 69 }, { name: 'Large', price: 89 }]
+  },
+  {
+    id: nextId++,
+    name: 'Currrly Fries',
+    sizes: [{ name: 'Small', price: 79 }, { name: 'Medium', price: 119 }, { name: 'Large', price: 149 }]
   },
   {
     id: nextId++,
@@ -20,5 +45,15 @@ export default [
       { name: 'Large', price: 99 },
       { name: 'Obscene', price: 139 }
     ]
+  },
+  {
+    id: nextId++,
+    name: 'Bottled Water',
+    sizes: [{ name: 'Regular', price: 129 }]
+  },
+  {
+    id: nextId++,
+    name: 'Milkshake',
+    sizes: [{ name: 'Small', price: 119 }, { name: 'Medium', price: 149 }, { name: 'Large', price: 179 }]
   }
 ]
