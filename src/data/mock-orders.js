d9a104a5d7411a098855b810f834aeba9f9e64cf
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-30 12:36:25 -0700

    Update IDs in mock data to avoid potential "key conflicts" when using mocks during dev

--- src/data/mock-orders.js
+++ src/data/mock-orders.js
@@ -5,7 +5,7 @@ A set of mock order data to use when needed to pre-populate the store/database/e
 export default {
   orders: [
     {
-      id: 1,
+      id: 99991,
       items: [
         { id: 1, name: 'Hamburger (Regular)', quantity: 1, pricePerUnit: 99, totalPrice: 99 },
         { id: 2, name: 'Hamburger (Regular)', quantity: 1, pricePerUnit: 99, totalPrice: 99 },
@@ -16,7 +16,7 @@ export default {
       totalPrice: 396
     },
     {
-      id: 3,
+      id: 99993,
       items: [
         { id: 5, name: 'Hamburger (Regular)', quantity: 1, pricePerUnit: 99, totalPrice: 99 },
         { id: 6, name: 'French Fries (Large)', quantity: 1, pricePerUnit: 89, totalPrice: 89 },
@@ -26,7 +26,7 @@ export default {
       totalPrice: 287
     },
     {
-      id: 5,
+      id: 99995,
       items: [
         { id: 8, name: 'Double Cheeseburger (Regular)', quantity: 1, pricePerUnit: 189, totalPrice: 189 },
         { id: 9, name: 'French Fries (Large)', quantity: 1, pricePerUnit: 89, totalPrice: 89 },
@@ -36,7 +36,7 @@ export default {
       totalPrice: 556
     },
     {
-      id: 7,
+      id: 99997,
       items: [
         { id: 11, name: 'Currrly Fries (Small)', quantity: 5, pricePerUnit: 79, totalPrice: 395 },
         { id: 12, name: 'Milkshake (Medium)', quantity: 5, pricePerUnit: 149, totalPrice: 745 }
@@ -45,7 +45,7 @@ export default {
       totalPrice: 1140
     },
     {
-      id: 9,
+      id: 99999,
       items: [
         { id: 13, name: 'Double Cheeseburger (Regular)', quantity: 2, pricePerUnit: 189, totalPrice: 378 },
         { id: 14, name: 'Bottled Water (Regular)', quantity: 2, pricePerUnit: 129, totalPrice: 258 }
