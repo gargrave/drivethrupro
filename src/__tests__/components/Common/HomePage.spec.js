d86437ce856ec804e213f928b8282fa0c7dcdcb2
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-30 16:04:48 -0700

    Refactor: rename Home.jsx -> HomePage.jsx

--- src/__tests__/components/Common/HomePage.spec.js
+++ src/__tests__/components/Common/HomePage.spec.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import Home, { Unwrapped } from '../../../components/Common/Home'
+import { Unwrapped } from '../../../components/Common/HomePage'
 
 describe('Home (without Redux)', () => {
   let props
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -4,12 +4,12 @@ import { Route, Switch } from 'react-router-dom'
 import OrderEditPage from './Orders/Edit/OrderEditPage'
 import OrdersListPage from './Orders/List/OrdersListPage'
 import NotFound from './Common/NotFound'
-import Home from './Common/Home'
+import HomePage from './Common/HomePage'
 
 const routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={HomePage} />
       <Route exact path="/orders/new" component={OrderEditPage} />
       <Route exact path="/orders/:id" component={OrderEditPage} />
       <Route exact path="/orders" component={OrdersListPage} />
