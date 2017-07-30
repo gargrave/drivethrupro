11c5f8ad6246a997363c0beade3b98f713e12a84
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-30 14:46:34 -0700

    Update 404/NoutFound page with some links

--- src/components/Common/NotFound.jsx
+++ src/components/Common/NotFound.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
     <div>
-      <h2>404 Not Found!</h2>
+      <h2>ONOS! This is the dreaded 404 page!</h2>
+      <h4>You are probably looking for one of these pages:</h4>
+      <h5>
+        <Link to="/orders">View Open Orders</Link>
+      </h5>
+      <h5>
+        <Link to="/orders/new">Create a New Order</Link>
+      </h5>
     </div>
   )
 }
